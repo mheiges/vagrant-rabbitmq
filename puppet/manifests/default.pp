1ed90497f3594afdb15c6f4dbcdee04218fa739a
Mark Heiges <mheiges@uga.edu>
2015-05-11 16:21:05 -0400

    refactor puppet custom firewall setup

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -2,13 +2,4 @@ Package {
   allow_virtual => true,
 }
 
-resources { "firewall":
-  purge   => true
-}
-
-Firewall {
-  before  => Class['fw::post'],
-  require => Class['fw::pre'],
-}
-
-class { ['fw::pre', 'fw::post']: }
+class { '::firewall_impl': }
